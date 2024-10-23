74ce7a2a8525a9f9d2020a23de704495ba3ddd57
tal <tal.getz.42@gmail.com>
2024-10-23 13:13:38 +0300

    research pywinauto netflix start

--- pywinauto_research.py
+++ pywinauto_research.py
@@ -1,4 +1,8 @@
 from pywinauto import Desktop
 
-app = Desktop(backend="uia").window(title="Netflix", visible_only=False)
+from pywinauto.application import Application
+app = Application(backend="uia").start('Netflix.exe')
+
+
+# app = Desktop(backend="uia").window(title="Netflix", visible_only=False)
 print(app.Properties.print_control_identifiers())
